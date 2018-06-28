cdc6b3092b98dc900da55f9822c932eb2cc2d88c
eva astudillo <eva.astudillox@gmail.com>
2018-06-28 06:22:46 -0400

    creacion de registro y modificacion de html

---
+++ app.js
@@ -0,0 +1,3 @@
+//var pfrecuentes = document.querySelector("index3.html").import;
+// seleccionar el elemento en la página importada
+//var rfrecuentes = contenido.querySelector("btn-dos");
---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('btnEnviarConsulta').addEventListener("click",cargarJson);
+
+function cargarJson(){
+const numeroTarjetaBip=document.getElementById("numeroTarjeta").value
+//console.log(numeroTarjetaBip)
+let url="";
+ url +="http://www.psep.cl/api/Bip.php?&numberBip=¨¨"
+ url+=`numberBip=${numeroTarjetaBip}`
+fetch(url)
+.then(resp =>resp.json())
+.then(data => {
+    //console.log(data)
+})
+.catch(e => console.log(e))
+}
